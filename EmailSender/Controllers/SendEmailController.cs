eca7932898978a8dedf259f281825c91f2d38bbe
ApilLamsal <apil.lamsal@riddhasoft.com>
2023-07-26 12:09:31 +0545

    Add project files.

---
+++ EmailSender/Controllers/SendEmailController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mail;
+using System.Net;
+using NuGet.Protocol.Plugins;
+
+namespace EmailSender.Controllers
+{
+    public class SendEmailController : Controller
+    {
+        public IActionResult Send()
+        {
+            return View();
+        }
+       [HttpPost]
+        public IActionResult Send(string To, string Subject, string Text)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var senderEmail = new MailAddress("Everestdogg@gmail.com");
+                    var receiverEmail = new MailAddress(To, "To");
+                    var password = "sqgyovpzzmhkjsrf";
+                    var sub = Subject;
+                    var body = Text;
+                    var smtp = new SmtpClient
+                    {
+                        Host = "smtp.gmail.com",
+                        Port = 587,
+                        EnableSsl = true,
+                        DeliveryMethod = SmtpDeliveryMethod.Network,
+                        UseDefaultCredentials = false,
+                        Credentials = new NetworkCredential(senderEmail.Address, password)
+                    };
+                    using (var mess = new MailMessage(senderEmail, receiverEmail)
+                    {
+                        Subject = Subject,
+                        Body = Text
+                    })
+                    {
+                        smtp.Send(mess);
+                        ViewBag.success = "Successfully sent..";
+                    }
+                    ModelState.Clear();
+                    return View();
+                    
+                }
+            }
+            catch (Exception)
+            {
+                ViewBag.Error = "Some Error";
+            }
+            return View();
+        }
+    }
+}
---
+++ EmailSender/Models/EmailModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Mail;
+
+namespace EmailSender.Models
+{
+    public class EmailModel
+    {
+        
+        [DisplayName("Email")]
+        [Required]
+        [EmailAddress]
+        public string To { get; set; }
+
+        [DisplayName("Subject")]
+        public string Subject { get; set; }
+
+        [DisplayName("Massage Text")]
+        public string Text { get; set; }
+
+        public void sendMail()
+        {
+            MailMessage mailMessage = new MailMessage("From", "To");
+        }
+    }
+}
